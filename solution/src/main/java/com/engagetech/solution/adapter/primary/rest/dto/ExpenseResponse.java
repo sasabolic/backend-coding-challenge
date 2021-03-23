abe7497e2051717f84aee6c39c77c390b36a6589
Saša Bolić <sasa.bolic@sixhours.io>
2021-03-23 21:22:02 +0100

    Refactor expense response

--- solution/src/main/java/com/engagetech/solution/adapter/primary/rest/dto/ExpenseResponse.java
+++ solution/src/main/java/com/engagetech/solution/adapter/primary/rest/dto/ExpenseResponse.java
@@ -1,37 +1,29 @@
 package com.engagetech.solution.adapter.primary.rest.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDate;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
  * Expense response DTO.
  */
 @Getter
+@AllArgsConstructor
 public class ExpenseResponse {
 
+  @JsonProperty("id")
   private Long id;
 
+  @JsonProperty("amount")
   private String amount;
 
+  @JsonProperty("vat")
   private String vat;
 
+  @JsonProperty("date")
   private LocalDate date;
 
+  @JsonProperty("reason")
   private String reason;
-
-  @JsonCreator
-  public ExpenseResponse(
-    @JsonProperty("id") Long id,
-    @JsonProperty("amount") String amount,
-    @JsonProperty("vat") String vat,
-    @JsonProperty("date") LocalDate date,
-    @JsonProperty("reason") String reason) {
-    this.id = id;
-    this.amount = amount;
-    this.vat = vat;
-    this.date = date;
-    this.reason = reason;
-  }
 }
